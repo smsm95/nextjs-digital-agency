f02c839fb048d525e7d144931f134a7df3a83a0e
smsm95 <osamamirghani95@gmail.com>
2021-05-14 22:55:49 +0400

    website initial design

---
+++ src/components/drawer.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from "react";
+import { Box } from "theme-ui";
+import RcDrawer from "rc-drawer";
+
+export default function Drawer({
+  className,
+  children,
+  closeButton,
+  closeButtonStyle,
+  drawerHandler,
+  toggleHandler,
+  open,
+  width,
+  placement,
+  drawerStyle,
+  closeBtnStyle,
+  ...props
+}) {
+  return (
+    <Fragment>
+      <RcDrawer
+        width={width}
+        open={open}
+        onClose={toggleHandler}
+        className={`drawer ${className || ""}`.trim()}
+        placement={placement}
+        handler={false}
+        level={null}
+        {...props}
+        duration={"0.4s"}>
+        {closeButton && (
+          <Box as="div" onClick={toggleHandler} sx={closeBtnStyle}>
+            {closeButton}
+          </Box>
+        )}
+        <Box sx={drawerStyle}>{children}</Box>
+      </RcDrawer>
+      <Box
+        className="drawer__handler"
+        style={{ display: "inline-block" }}
+        onClick={toggleHandler}>
+        {drawerHandler}
+      </Box>
+    </Fragment>
+  );
+}
+
+Drawer.defaultProps = {
+  width: "320px",
+  placement: "left",
+};
